c51e44204ba1ac97c9f9d56d4fdf5360199b068c
Richard O. Gregory <richardgrecoson@gmail.com>
2019-11-27 16:44:57 +0100

    Add personal styling.

--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 import './search-box.css';
 
 export const SearchBox = ({ placeholder, handleChange }) => (
-    <input
-      className='search'
-      type='search'
-      placeholder={ placeholder }
-      onChange={ handleChange }
-    />
+  <div id='cover'>
+    <div className='tb'>
+      <div className='td'>
+        <input
+          className='search'
+          type='search'
+          placeholder={ placeholder }
+          onChange={ handleChange }
+        />
+      </div>
+    </div>
+  </div>
+    
 );
