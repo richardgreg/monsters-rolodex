d27079ecd26d85d0b0e61470b5ce0d860441b7ca
Richard O. Gregory <richardgrecoson@gmail.com>
2019-11-27 17:24:50 +0100

    Updates

---
+++ precache-manifest.ceedbd71b93a9aa11faa5676da1c8f49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151dd990a7595926a48818cf87fcd9b3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "62b19ee6f59ce6e11b94",
+    "url": "/monsters-rolodex/static/css/main.b6e83ac2.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "62b19ee6f59ce6e11b94",
+    "url": "/monsters-rolodex/static/js/main.eb25dc56.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
